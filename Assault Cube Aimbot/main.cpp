45d43f8344de30e362ab4feed9170847478150f8
A3xtr <77483547+A3xtra@users.noreply.github.com>
2025-07-01 16:51:33 +0100

    Aim bot code, aims on head if within 20 distance

--- Assault Cube Aimbot/main.cpp
+++ Assault Cube Aimbot/main.cpp
@@ -17,7 +17,7 @@
 const float PI = 3.14159265358979323846;
 
 // debug mode
-bool b_debugEnabled = true;
+bool b_debugEnabled = false;
 
 uintptr_t uint_PID = 0;
 
@@ -34,6 +34,12 @@ int entityCount = 0;
 // lpBuffer - buffer variable to store the value in
 // nSize - number of bytes to read from specified process
 // lpNumberOfBytesRead - A pointer to a variable that receives the number of bytes transferred into the specified buffer. If lpNumberOfBytesRead is NULL, the parameter is ignored.
+
+//WriteProcessMemory - [in] HANDLE hProcess, [in] LPVOID lpBaseAddress, [in] LPCVOID lpBuffer, [in] SIZE_T nSize, [out] SIZE_T *lpNumberOfBytesWritten
+// hProcess - A handle to the process memory to be modified. The handle must have PROCESS_VM_WRITE and PROCESS_VM_OPERATION access to the process.
+// lpBaseAddress - pointer to the base address in the specified process to which data is being written
+// nSize - number of bytes being written to the process
+// lpNumberOfBytesWritten - a pointer to a variable that receives the number of bytes transferred into the specified process, optional
 
 struct player
 {
@@ -55,15 +61,48 @@ struct player
 		ReadProcessMemory(h_Process, (LPCVOID)(localPlayerPtr + camPosOffset), &pos, sizeof(pos), 0);
 		ReadProcessMemory(h_Process, (LPCVOID)(localPlayerPtr + nameOffset), &username, sizeof(username), 0);
 
-		ReadProcessMemory(h_Process, (LPCVOID)(uint_ac_client_exe + playerCountAddr), &entityCount, sizeof(entityCount), 0);
-		// total entities in the game minus 1, player isn't in entity list.
-		--entityCount;
+		if (!ReadProcessMemory(h_Process, (LPCVOID)(uint_ac_client_exe + playerCountAddr), &entityCount, sizeof(entityCount), 0)) {
+			std::cerr << "[ERROR] Failed to read entity count from memory." << std::endl;
+			entityCount = 0;
+		}
+		else {
+			// total entities in the game minus 1, player isn't in entity list.
+
+			--entityCount;
+		}
+
 	
 		ReadProcessMemory(h_Process, (LPCVOID)(uint_ac_client_exe + gameStateAddr), &gameState, sizeof(gameState), 0);
 		//std::cout << localPlayerPtr;
 
 	}
 }Player;
+
+struct entityList
+{
+	uintptr_t entityBasePtr = 0;
+	int team = 0; // team ID
+	int health = 0; // player health
+	float pos[3] = { 0,0,0 }; // player pos
+	float aimbotAngle[3] = { 0,0,0 };
+	char username[20]; // player username
+
+
+	// Read entity info from memory, loop through entity list
+	void ReadMemory(int entityNum)
+	{
+		entityNum++;
+		// Read memory function that just reads all adresses + offest and stores them in the variable's addresses
+		ReadProcessMemory(h_Process, (LPCVOID)(uint_ac_client_exe + entListBaseAddr), &entityBasePtr, sizeof(entityBasePtr), 0);
+		ReadProcessMemory(h_Process, (LPCVOID)(entityBasePtr+(entityNum*entStep)), &entityBasePtr, sizeof(entityBasePtr), 0);
+		ReadProcessMemory(h_Process, (LPCVOID)(entityBasePtr + teamOffset), &team, sizeof(team), 0);
+		ReadProcessMemory(h_Process, (LPCVOID)(entityBasePtr + healthOffset), &health, sizeof(health), 0);
+		ReadProcessMemory(h_Process, (LPCVOID)(entityBasePtr + camPosOffset), &pos, sizeof(pos), 0);
+		ReadProcessMemory(h_Process, (LPCVOID)(entityBasePtr + nameOffset), &username, sizeof(username), 0);
+
+	}
+};
+
 
 void DebugCalculateAngle()
 {
@@ -98,17 +137,195 @@ void DebugCalculateAngle()
 	Angles[1] *= (180 / PI);
 
 	// clearing console for continuous updates
-	system("CLS");
-	std::cout << "deltaX = " << deltaX << std::endl;
-	std::cout << "deltaY = " << deltaY << std::endl;
-	std::cout << "deltaZ = " << deltaZ << std::endl;
-	std::cout << "Hypotenuse = " << hypotenuse << std::endl;
-	std::cout << "Yaw = " << Angles[0] << std::endl;
-	std::cout << "Pitch = " << Angles[1] << std::endl;
-	std::cout << "Player pos: (" << Player.pos[0] << ", " << Player.pos[1] << ", " << Player.pos[2] << ")" << std::endl;
+	//std::cout << "deltaX = " << deltaX << std::endl;
+	//std::cout << "deltaY = " << deltaY << std::endl;
+	//std::cout << "deltaZ = " << deltaZ << std::endl;
+	//std::cout << "Hypotenuse = " << hypotenuse << std::endl;
+	//std::cout << "Yaw = " << Angles[0] << std::endl;
+	//std::cout << "Pitch = " << Angles[1] << std::endl;
+	//std::cout << "Player pos: (" << Player.pos[0] << ", " << Player.pos[1] << ", " << Player.pos[2] << ")" << std::endl;
 
-	WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + rotationOffset), &Angles, sizeof(Angles), 0); // writes both yaw and pitch
+	//WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + rotationOffset), &Angles, sizeof(Angles), 0); // writes both yaw and pitch
 }
+
+
+
+void CalculateAngle(float* Player, float* Enemy, float* Angles)
+{
+	// diff in x,y,z coords between target and player.
+	float deltaX = Enemy[0] - Player[0];
+	float deltaY = Enemy[1] - Player[1];
+	float deltaZ = Enemy[2] - Player[2];
+
+	// 3d hypotenuse
+	float hypotenuse = sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+
+	// trig: tan o/a, sin o/h, cos a/h
+
+	// Calculate the yaw (vertical angle, left and right) using arctangent2 function
+	//atan2f(y,x) represents an angle in a right triangle, y is the opposite side.
+	// x is the adjacent side.
+	// so function is atan2f(opposite,adjacent)
+	Angles[0] = (atan2f(deltaY, deltaX));
+
+	Angles[0] *= (180 / PI); // convert radians to degrees
+
+	Angles[0] += 90.0f; // Add 90 degrees to yaw, because of AC's coord system
+
+	// calculate pitch using arcsin
+	// pitch angle is arcsin(opp/hyp)
+	Angles[1] = (asinf(deltaZ / hypotenuse));
+	Angles[1] *= (180 / PI);
+}
+
+// list of targets, distance for each, angle for each
+struct targetList
+{
+	float distance = 0; // distance from player to target;
+	float aimAngles[3] = { 0,0,0 }; // angles for aiming
+	char* name = 0; // name of enemy aimed at
+	int health = 0; // enemy target health
+	int team = 0; // enemy target team ID
+	uintptr_t entityBasePtr = 0; // pointer to target's memory location
+
+	targetList() {} // empty constructor
+
+	// initalising constructor
+	targetList(float aimbotAngles[], float playerCoords[], float enemyCoords[], char enemyName[], int enemyHealth, int enemyTeam, uintptr_t enemyEntityBasePtr)
+	{
+		// calculate distance for target using 3d pythag
+		distance = Get3dDistance(playerCoords[0], playerCoords[1], playerCoords[2], enemyCoords[0], enemyCoords[1], enemyCoords[2]);
+
+		aimAngles[0] = aimbotAngles[0];
+		aimAngles[1] = aimbotAngles[1];
+		aimAngles[2] = aimbotAngles[2];
+		name = enemyName;
+		health = enemyHealth;
+		team = enemyTeam;
+		entityBasePtr = enemyEntityBasePtr;
+	}
+
+	float Get3dDistance(float myCoordsX, float myCoordsY, float myCoordsZ, float enemyCoordsX, float enemyCoordsY, float enemyCoordsZ)
+	{
+		return sqrt(pow(float(enemyCoordsX - myCoordsX), 2) + pow(float(enemyCoordsY - myCoordsY), 2) + pow(float(enemyCoordsZ - myCoordsZ), 2));
+	}
+};
+
+
+void Aimbot()
+{
+	Player.ReadMemory();
+
+	// dynamically allocate memory for target list and entity list
+	if (entityCount <= 0) 
+	{
+		std::cout << "Invalid entity count: " << entityCount << std::endl;
+		return;
+	}
+	targetList* targetListPtr = new targetList[entityCount]; // raw pointer, array, when using 'new' delete after using it to prevent memory leaks
+	std::vector<entityList> entityList(entityCount); // smart pointer, vector
+
+	int targetLoop = 0;
+
+	for (int i = 0; i < entityCount; i++)
+	{
+		entityList[i].ReadMemory(i);
+
+		if (entityList[i].health <= 0) continue;
+
+		// gamemode IDs
+
+		if (Player.gameState == 7 && entityList[i].team == Player.team) continue;
+		if (Player.gameState == 20 && entityList[i].team == Player.team) continue;
+		if (Player.gameState == 21 && entityList[i].team == Player.team) continue;
+
+		CalculateAngle(Player.pos, entityList[i].pos, entityList[i].aimbotAngle);
+
+		targetListPtr[targetLoop] = targetList(
+			entityList[i].aimbotAngle,
+			Player.pos,
+			entityList[i].pos,
+			entityList[i].username,
+			entityList[i].health,
+			entityList[i].team,
+			entityList[i].entityBasePtr
+		);
+
+		targetLoop++;
+	}
+
+	if (targetLoop == 0)
+	{
+		system("CLS");
+		printf("No valid targets found. \n");
+	}
+
+	if (targetLoop > 0)
+	{
+		std::sort(
+			targetListPtr, targetListPtr + targetLoop,
+			[](const targetList& a, const targetList& b)
+			{
+				return a.distance < b.distance;
+			});
+
+		if (b_debugEnabled)
+		{
+			printf("Aimbot always on, hold right mouse to disable aimbot, F6 to quit. \n");
+			printf("Num pad 9 to toggle debug output. \n");
+			std::cout << "Debug: " << (b_debugEnabled == true ? "ON\n" : "OFF\n") << std::endl;
+
+			std::cout << (
+				Player.gameState == 0 ? "Default match" :
+				Player.gameState == 7 ? "TDM / Survival" :
+				Player.gameState == 8 ? "Deathmatch" :
+				Player.gameState == 12 ? "One shot, one kill" :
+				Player.gameState == 18 ? "Pistol frenzy" :
+				Player.gameState == 19 ? "LMS" :
+				Player.gameState == 20 ? "Team survivor" :
+				Player.gameState == 21 ? "Team one shot, one kill" :
+				"Unknown game mode") << " - Number of entities: " << entityCount << " - PIDwwwwww: " << uint_PID << " - targetLoop: " << targetLoop << "\n" << std::endl;
+
+			std::string alignment = "";
+
+			if (Player.health <= -1 && Player.health >= -9) { alignment = "          "; }
+			if (Player.health <= -10 && Player.health >= -99) { alignment = "          "; }
+			if (Player.health <= 0 && Player.health >= 9) { alignment = "          "; }
+			if (Player.health <= 10 && Player.health >= 99) { alignment = "          "; }
+			if (Player.health >= 100) { alignment = "          "; }
+
+			// display player info
+			std::cout << alignment << "                   " << Player.health << "                   " << Player.team << "     " << Player.username << std::endl;
+
+			std::cout << "Number - Ent address - health - distance - team - name\n";
+			int iNum = 0;
+			for (int i = 0; i < targetLoop; i++)
+			{
+				++iNum;
+
+				std::cout << (iNum < 10 ? " " : "") << iNum
+					<< "      0x" << std::hex << std::setw(8) << std::setfill('0') << targetListPtr[i].entityBasePtr << std::dec
+					<< "     " << (targetListPtr[i].health < 10 ? " " : "") << (targetListPtr[i].health < 100 ? " " : "") << targetListPtr[i].health << "             "
+					<< (static_cast<int>(targetListPtr[i].distance) < 10 ? " " : "") << (static_cast<int>(targetListPtr[i].distance) < 100 ? " " : "") << static_cast<int>(targetListPtr[i].distance)
+					<< (targetListPtr[i].team < 10 ? " " : "") << targetListPtr[i].team << "   "
+					<< targetListPtr[i].name 
+					<< std::endl;
+			}
+		}
+
+		if (!GetAsyncKeyState(Right_Mouse) && targetListPtr[0].distance < 20)
+		{
+
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + rotationOffset), targetListPtr[0].aimAngles, sizeof(targetListPtr[0].aimAngles), 0);
+		}
+
+	}
+
+	//clean up dynamically allocated memory
+	targetLoop = 0;
+	delete[] targetListPtr;
+}
+
 
 int main()
 {
@@ -129,22 +346,42 @@ int main()
 	while (!GetAsyncKeyState(F6))
 	{
 		system("CLS");
-		printf("F6 to quit, num pad 9 to debug. \n");
+		printf("F1 - health, F2 - ammo, F6 to quit, F9 to debug. \n");
 		std::cout << "Debug = " << (b_debugEnabled == true ? "ON" : "OFF") << std::endl;
 
 	
-		if(GetAsyncKeyState(Number_Pad_9) & 0x8000)
+		// F9 KEY
+		if(GetAsyncKeyState(0x78) & 0x8000)
 		{ 
 			b_debugEnabled = !b_debugEnabled;
 			Sleep(100);
 		}
-		if (b_debugEnabled)
+		// F1 - health
+		if (GetAsyncKeyState(0x70) & 0x8000)
 		{
-			DebugCalculateAngle();
+			int newHealth = 9999; // amount to be written to the pointer
+			//writes to the health address the amount at the address of newHealth
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + healthOffset), &newHealth, sizeof(newHealth), 0);
 		}
+		//F2 - ammo
+		if (GetAsyncKeyState(0x71) & 0x8000)
+		{
+			int newAmmo = 9999; // amount to be written to the pointer
+			//writes to the ammo addresses the amount at the address of newAmmo
 
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + mgAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + mgAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + carbineAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + shotgunAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + sniperAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + arifleAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+			WriteProcessMemory(h_Process, (BYTE*)(Player.localPlayerPtr + pistolAmmoOffset), &newAmmo, sizeof(newAmmo), 0);
+		}
+		Aimbot();
+		Sleep(1);
 	}
 	CloseHandle(h_Process);
 	return 0;
 }
+
 
--- Assault Cube Aimbot/offsets.h
+++ Assault Cube Aimbot/offsets.h
@@ -7,6 +7,14 @@ const uintptr_t entStep = 0x4; //gap between entities in bytes //uint_entityloop
 const uintptr_t playerCountAddr = 0x10F500; //uint_playercount
 const uintptr_t gameStateAddr = 0x10A044; //uint_gamestate
 const uintptr_t camPosOffset = 0x4; // aiming at head? //uint_eye_position
+
+const uintptr_t mgAmmoOffset = 0x148; // machine gun ammo
+const uintptr_t carbineAmmoOffset = 0x140; // carbine ammo
+const uintptr_t shotgunAmmoOffset = 0x144; // shot gun ammo
+const uintptr_t sniperAmmoOffset = 0x14C; // sniper ammo
+const uintptr_t arifleAmmoOffset = 0x150; // rifle ammo
+const uintptr_t pistolAmmoOffset = 0x13C; // pistol ammo
+
 const uintptr_t rotationOffset = 0x40; // yaw and pitch //uint_rotation
 const uintptr_t healthOffset = 0xF8; //uint_health
 const uintptr_t nameOffset = 0x225; // uint_name
